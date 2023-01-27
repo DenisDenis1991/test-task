324bbb1843c1f569801e1086f9719eb2cba5de10
DenisDenis1991 <93926317+DenisDenis1991@users.noreply.github.com>
2023-01-27 13:36:39 +0300

    Рефакторинг

--- source/js/main.js
+++ source/js/main.js
@@ -1,29 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
 
   const formTable = document.querySelector('.form__wrapper');
-  const addTableRowButton = document.querySelector('.button-add');
   const tableRow = document.getElementById('table-row').content;
   const form = document.querySelector('.form');
 
-  addTableRowButton.addEventListener('click', () => {
-    formTable.append(tableRow.cloneNode(true));
-    const arrayBox = formTable.querySelectorAll('.form__box');
-    for (let i = 1; i <= arrayBox.length - 1; i++) {
-      arrayBox[i].setAttribute('data-number', i);
+  const buttonForm = (evt) => {
+    if (evt.target.classList.contains('button-add')) {
+      formTable.appendChild(tableRow.cloneNode(true));
     }
-    const arrBtn = formTable.querySelectorAll('.form__delete');
-    for (let j = 1; j <= arrBtn.length - 1; j++) {
-      arrBtn[j].setAttribute('data-btn', j);
+    if (evt.target.classList.contains('form__delete')) {
+      evt.target.parentElement.remove();
     }
-  });
+  };
 
-  formTable.addEventListener('click', (evt) => {
-    const currentButton = evt.target.dataset.btn;
-    if (currentButton) {
-      const currentDeleteButton = formTable.querySelector(`[data-number='${currentButton}']`);
-      currentDeleteButton.remove();
-    }
-  });
+  form.addEventListener('click', buttonForm);
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
